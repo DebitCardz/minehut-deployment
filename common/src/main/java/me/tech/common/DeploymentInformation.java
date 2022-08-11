52d38b368fa673d6403242a97fa7e0a4160c9af3
Tech <debitcardzpaypal@gmail.com>
2022-08-11 07:42:27 -0400

    Github deployment is now optional

--- common/src/main/java/me/tech/common/DeploymentInformation.java
+++ common/src/main/java/me/tech/common/DeploymentInformation.java
@@ -19,16 +19,26 @@ public class DeploymentInformation {
     private final String jarName;
 
     public DeploymentInformation(Map<String, String> settings) {
-        this.token = settings.get("token");
-        this.organization = settings.get("organization");
-        this.repository = settings.get("repository");
-        this.branch = settings.get("branch");
-        this.serverName = settings.get("server_name");
-        this.webhookUrl = settings.get("webhook_url");
-        this.jarName = settings.get("jar_name");
+        this.token = settings.getOrDefault("token", null);
+        this.organization = settings.getOrDefault("organization", null);
+        this.repository = settings.getOrDefault("repository", null);
+        this.branch = settings.getOrDefault("branch", null);
+        this.serverName = settings.getOrDefault("server_name", null);
+        this.webhookUrl = settings.getOrDefault("webhook_url", null);
+        this.jarName = settings.getOrDefault("jar_name", "server.jar");
     }
 
     public Optional<String> buildPythonCommand() {
+        // Github deployment not setup, just don't use it.
+        if(
+                token == null
+                || organization == null
+                || repository == null
+                || branch == null
+        ) {
+            return Optional.empty();
+        }
+
         return Optional.of(String.format(
                 "python3 %s/deployment.py -token %s -organization %s -repository %s -branch %s -servername %s -webhook %s",
                 Deployment.SERVER_DIRECTORY,
